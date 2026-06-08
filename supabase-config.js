c627ff18c372092b563a265f9401bf4bc5c99f5e
kuhnnic <129492916+kuhnnic@users.noreply.github.com>
2026-06-08 09:29:28 +0200

    Set SASIS hospital table in Supabase config

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
 window.CMCSIX_SUPABASE_CONFIG = {
   url: 'https://wlmsbsxgzmkkhwsimcdj.supabase.co',
   anon: 'sb_publishable_ifDX1hz1pkNcOdrIrg5icw_uVMK11IP',
-  table: 'beds'
+  table: 'beds',
+  hospitalTable: 'sasis_hospitals'
 };
