c10ff5b03e81b6d975a5b04098ef1584631a4248
kuhnnic <129492916+kuhnnic@users.noreply.github.com>
2026-06-08 15:15:13 +0200

    Bust app cache for multi status filter

--- assets/main-db-loader.js
+++ assets/main-db-loader.js
@@ -2,7 +2,7 @@ window.CMCSIX_SUPABASE_CONFIG = window.CMCSIX_SUPABASE_CONFIG || {};
 window.CMCSIX_SUPABASE_CONFIG.hospitalTable = 'sasis_hospitals_api';
 
 const cmcsixScript = document.createElement('script');
-cmcsixScript.src = 'assets/main-db.js?v=20260608-room-dropdown-v2';
+cmcsixScript.src = 'assets/main-db.js?v=20260608-multi-status-v1';
 cmcsixScript.async = false;
 cmcsixScript.onload = () => {
   if (typeof window.init === 'function') window.init();
